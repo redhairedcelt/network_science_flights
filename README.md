a1654c0a7acdc2c7d4889a471ab02bee714a15c0
Patrick <patrickfmaus@gwu.edu>
2020-02-01 23:03:42 -0500

    initial commit of EDA with data and readme notes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # network_science_flights
+
+## Initial EDA
+- Times are read in as integers.  Need to be joined with the date to be used at date times.
+- All values are 1 for Flights, confirming this dataset is for unique flights each day.
+- Cancelled and diverted are so rare, we likely dont need them as columns
+- Distance can be used for comparing to future GIS solutions or plots.  Could also be a good metric for each different airline or day to see how many miles were covered in total.
+- WN is the most numerous airline, and it stands for Southwest.
+
+## Some Questions to explore
+- Look at flights by day, and maybe eventually by month.  What are the trends?  Are there differences across different airline or at different airports?
+- Do different airlines operate more or less are different airports?
---
+++ eda_maus.R
@@ -0,0 +1,55 @@
+# This function will install any packages you dont have installed already.
+loadPkg = function(x) { if 
+  (!require(x,character.only=T, quietly =T)) 
+{ install.packages(x,dep=T,repos="http://cran.us.r-project.org"); 
+    if(!require(x,character.only=T)) stop("Package not found") } }
+
+loadPkg('dplyr')
+library(dplyr)
+loadPkg('readr')
+library(readr)
+
+loadPkg("igraph") 
+library(igraph) # Load the igraph package
+loadPkg("networkD3") 
+library(networkD3)
+loadPkg("threejs") 
+library(threejs)
+
+loadPkg('maps')
+library(maps)
+loadPkg('geosphere')
+library(geosphere)
+
+df <- read_csv('data/ontime_rpt_201901.csv')
+
+str(df)
+summary(df)
+head(df)
+
+
+## Lets try to build a network from edges
+edges <- select(df, DEST, ORIGIN)
+head(edges)
+flight_graph <- graph_from_data_frame(edges, directed=T)
+
+# Let's look at the degree of some airports
+plot(degree(flight_graph))
+max(degree(flight_graph))
+hist(degree(flight_graph))
+# Really right-tailed distribution.  Scale-free?
+
+# Whole network kept freezing my computer, so lets just plot the first 10,000 edges
+sample_edges <- graph_from_data_frame(edges[1:10000,], directed=T)
+plot(sample_edges)
+
+
+# Plot on a map.  Once I get the geo coords in, we can start messing around with visuals. 
+# I'd like to do a time lapse to show the flights over days/months with different colors
+# for different attributes, such as airline or how early/late each flight is.
+map("state", col="grey20", fill=TRUE, bg="black", lwd=0.1)
+
+
+
+
+
