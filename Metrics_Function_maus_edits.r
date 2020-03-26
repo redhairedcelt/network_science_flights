5729ac7b64b75dbb123c735983e3dfcf6e859e8c
Patrick <patrickfmaus@gwu.edu>
2020-03-26 12:44:39 -0400

    updates to metrics

--- Metrics_Function_maus_edits.r
+++ Metrics_Function_maus_edits.r
@@ -22,13 +22,6 @@ get_metrics <- function(dataframe) {
   # Can change to city instead of city code if we want
   g <- graph_from_edgelist(dataframe %>% select(ORIGIN, DEST) %>% as.matrix())
   
-  # CENTRALITY
-  # An example of a local centrality measure is the degree centrality, 
-  # which counts the number of links held by each node and points at individuals 
-  # who can quickly connect with the wider network
-  degree.cent <- centr_degree(g, mode = "all")
-  Degree_Centrality = mean(degree.cent$res)
-  
   # It calculates the shortest paths between all nodes, 
   # then assigns each node a score based on its sum of shortest paths 
   # and is useful for finding the individuals who are best placed to influence 
@@ -84,17 +77,24 @@ metrics_american_2018 = get_metrics(american_2018)
 metrics_delta_2018 = get_metrics(delta_2018)
 metrics_united_2018 = get_metrics(united_2018)
 
+#troubleshoot sw
+g <- graph_from_edgelist(southwest_2018 %>% select(ORIGIN, DEST) %>% as.matrix())
+numb_edges <- length(E(g))
+numb_vertices <-length(V(g))
+p_edges <- (numb_edges/(numb_vertices*(numb_vertices-1)))
 
+print(p_edges)
+print(numb_vertices)
 
 summary_metrics <- data.frame()
 metric_dfs <- list(
                 metrics_1998,
-                
+                metrics_2018,
                 metrics_american_2018,
                 metrics_delta_2018,
                 metrics_united_2018)
 
-row_names <- c(
+row_names <- c(    'metrics_2018',
                    'metrics_1998',
                    
                    'metrics_american_2018',
@@ -110,7 +110,8 @@ for (df in metric_dfs) {
 row.names(summary_metrics) <- row_names
 colnames(summary_metrics) <- col_names
 
-#write.csv(summary_metrics, 'summary_metrics.csv')
+summary_metrics <- round(summary_metrics, digits=5)
+write.csv(summary_metrics, 'summary_metrics.csv')
 
 # for non averaged metrics output
 #for (df in metric_dfs) {
@@ -118,100 +119,3 @@ colnames(summary_metrics) <- col_names
 #  }
 #row.names(summary_metrics) <- dfs_names
 #colnames(summary_metrics) <- col_names
-
-library(ggplot2)
-library(maps)
-library(ggmap)
-
-plot_network = function (df_map, point_color = 'blue', line_color = point_color,
-                         map_title = 'Top Routes', background_fill = 'grey',
-                         map_state_lines = 'black', map_fill = 'black' ) 
-{
-  
-  df_map <- subset(df_map, ORIGIN!=DEST)
-  df_map <- na.omit(df_map)
-  df_map <-subset(df_map, origin_lat!=dest_lat)
-  
-  # scaling function.  Could use some work.
-  range01 <- function(x){(x+1-min(x))/(max(x)-min(x))}
-  #range01 <- function(x){(log(x)-min(log(x)))/(max(log(x))-min(log(x)))}
-  # need to plot state lines
-  states_map <- map_data("state")  
-  
-  # Make a color pallate between two colors.  Grey is default first color.
-  col.1 <- adjustcolor('grey', alpha=0.4)
-  col.2 <- adjustcolor(line_color, alpha=.8)
-  edge.pal <- colorRampPalette(c(col.1, col.2), alpha = TRUE)
-  edge.col <- edge.pal(10)
-  # add a column for color indexing scaled by number of flights on each leg.
-  # make sure any 0 are set to minimum of 1, because R starts at 1.  Argh.
-  df_map$color_ind <- round(range01(df_map$flights)*10)
-  df_map$color_ind[df_map$color_ind==0] <- 1
-  
-  # add and scale total number of flights
-  df_map$total_flts_scaled <- range01(df_map$flights)
-  
-  # find counts of flights leaving by iata code, add to df_map as airport counts
-  airport_counts_df <- as.data.frame(df_map %>% group_by(ORIGIN) %>% 
-                                       summarise(counts = n()))
-  df_map <- merge(df_map, airport_counts_df, by='ORIGIN')
-  # scale and add back to df
-  df_map$airpot_counts_scaled <- range01(df_map$counts)
-  
-  # actual plotting
-  gg <- ggplot()
-  gg <- gg + geom_map(data=states_map, map=states_map, aes(map_id=region),
-                      color=map_state_lines, fill=map_fill, size=0.25) +
-    expand_limits(x=states_map$long, y=states_map$lat)
-  gg <- gg + labs(x=NULL, y=NULL, title=map_title) + 
-    theme_void() + # Empty theme without axis lines and texts
-    theme(panel.background = element_rect(fill=background_fill, colour=background_fill),
-          plot.background = element_rect(fill=background_fill, color=background_fill))
-  #coord_map("albers", lat0=39, lat1=49) +
-  #coord_map("albers", lat0=39, lat1=49) +
-  gg <- gg +
-    # The geom points are plotted scaled 0 to 1.  The factor can be adjusted 
-    geom_point(data=df_map, aes(x=origin_lon, y=origin_lat), 
-               #col=point_color, 
-               shape = 21, colour = "black", fill = point_color,
-               size=(df_map$airpot_counts_scaled)*4) +
-    geom_curve(data = df_map, 
-               aes(x=origin_lon, y=origin_lat, xend=dest_lon, yend=dest_lat), 
-               # color is selected by index from the predefined color pallete above
-               # needs to be rounded to an int between 1 and 10
-               col = edge.col[df_map$color_ind], 
-               # size is scaled 0 to 1
-               size = (df_map$total_flts_scaled),
-               curvature = 0.3, angle = 90, ncp = 5)
-  gg
-}
-
-dataframe <- united_2018
-g <- graph_from_edgelist(dataframe %>% select(ORIGIN, DEST) %>% as.matrix())
-#get number of vertices, edges, and p(edge) for random networks
-numb_edges <- length(E(g))
-numb_vertices <-length(V(g))
-p_edges <- (numb_edges/(numb_vertices*(numb_vertices-1)))
-g_rand <- sample_gnp(n=numb_vertices, p=p_edges, directed=TRUE)
-plot(g_rand)
-
-# translate to a df
-g_rand_df <- as_long_data_frame(g_rand)
-head(g_rand_df)
-# read the lat/lon for each airport
-origin <- fread('cont_us_nodes.csv')
-names(origin) <- c("ORIGIN", 'origin_lat', 'origin_lon')
-
-dest <- fread('cont_us_nodes.csv')
-names(dest) <- c("DEST", 'dest_lat', 'dest_lon')
-
-# define the grouping columns
-cols <- c("ORIGIN", "ORIGIN_CITY_NAME", "DEST", "DEST_CITY_NAME", "UNIQUE_CARRIER")
-
-
-
-plot_network(df_map=DL_1998, map_title="Delta in 1998", point_color='blue')
-
-
-
-
